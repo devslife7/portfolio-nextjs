c84c776e84b29deb39d083a0c6eec931c86a319c
Marcos Velasco <devslife7@gmail.com>
2024-01-10 02:00:43 -0500

    adding grid layout to contact page

--- components/sections/about.tsx
+++ components/sections/about.tsx
@@ -26,12 +26,13 @@ export default function About() {
           Ab<span className="text-primary">ou</span>t
         </h1>
         <div className="space-y-10">
-          <p className="max-w-[850px] leading-[1.9rem] text-lg">
+          <p className="max-w-[850px] leading-[1.9rem] text-lg font-light">
             Hello, I&apos;m Marcos Velasco and I&apos;m a Full Stack Software Developer. I have experience in building
             Full Stack Ruby on Rails and Javascript based web applications, and RESTful API architecture. Possess strong
             skill in problem solving and teamwork. Excited by challenging work and enjoy creating sites that focus on
             usefulness and elegance.
           </p>
+          <div className="flex gap-6 mb-5 md:mb-10">{renderSocialLinks()}</div>
           <div>
             <Link
               href="https://docs.google.com/document/d/17uI6lYjB1E6AioHoP3Cc6fBPmfFkutp2KXSefHKYsa8/edit?usp=sharing"
@@ -44,7 +45,6 @@ export default function About() {
               </div>
             </Link>
           </div>
-          <div className="flex gap-6 mb-5 md:mb-10">{renderSocialLinks()}</div>
         </div>
       </div>
     </section>
--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -1,35 +1,36 @@
 export default function Contact() {
   return (
-    <section id="contact" className=" bg-yellow-400">
-      <h1>
+    <section id="contact" className=" bg-gray-100 py-28 my-container">
+      <h1 className="text-center text-5xl mb-20">
         C<span className="text-primary">on</span>tact
       </h1>
 
-      <div>
-        <h2>Get in Touch</h2>
-        <p>
-          I'm excited to connect with new people or answer any questions you might have. Easily send me a message using
-          the following form including your email, and I will be in touch as soon as possible.
-        </p>
-        <h3>Name</h3>
-        <p>Marcos Velasco</p>
-        <h3>Location</h3>
-        <p>Alexandria, VA</p>
-        <h3>Email</h3>
-        <p>devslife@gmail.com</p>
-      </div>
-
-      <div>
-        <h2>Message me</h2>
-        <form>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" />
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" />
-          <label htmlFor="message">Message</label>
-          <textarea id="message" name="message" />
-          <button type="submit">Submit</button>
-        </form>
+      <div className="grid grid-cols-2 gap-4 bg-slate-600">
+        <div className="bg-red-400">
+          <h2>Get in Touch</h2>
+          <p>
+            I'm excited to connect with new people or answer any questions you might have. Easily send me a message
+            using the following form including your email, and I will be in touch as soon as possible.
+          </p>
+          <h3>Name</h3>
+          <p>Marcos Velasco</p>
+          <h3>Location</h3>
+          <p>Alexandria, VA</p>
+          <h3>Email</h3>
+          <p>devslife@gmail.com</p>
+        </div>
+        <div className="bg-yellow-300">
+          <h2>Message me</h2>
+          <form>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message" />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
     </section>
   )
