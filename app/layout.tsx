a0c56558902fce2d4fe95f880dca9bf2c2b68bc8
Marcos Velasco <devslife7@gmail.com>
2024-01-16 20:36:39 -0500

    stripped down version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,22 @@
 import type { Metadata } from "next"
 import { Ubuntu } from "next/font/google"
-import "./globals.css"
+// import "./globals.css"
 import Navbar from "@/components/layout/Navbar"
 import Footer from "@/components/layout/Footer"
 import { Toaster } from "sonner"
 
-const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["300", "400", "500", "700"] })
+// const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["300", "400", "500", "700"] })
 
-export const metadata: Metadata = {
-  title: "Marcos' Portfolio",
-  description: "Personal portfolio of projects.",
-}
+// export const metadata: Metadata = {
+//   title: "Marcos' Portfolio",
+//   description: "Personal portfolio of projects.",
+// }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={ubuntu.className}>
+      {/* <body className={ubuntu.className}> */}
+      <body>
         {/* <Toaster richColors position="top-center" expand />
         <Navbar />
         {children}
