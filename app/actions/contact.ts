176cd587c1946077c48ffa92019dcf1cddcb53c0
Marcos Velasco <devslife7@gmail.com>
2024-01-16 20:50:39 -0500

    commented out server actions

--- app/actions/contact.ts
+++ app/actions/contact.ts
@@ -21,35 +21,35 @@ export default async function sendMessage(formData: unknown) {
   }
 
   // verify recaptcha
-  const captchaResp = await verifyReCaptcha(validated.data.recaptcha)
+  // const captchaResp = await verifyReCaptcha(validated.data.recaptcha)
   // if (!captchaResp.success) return captchaResp
 
   // send email using the resend api
-  const { name, email, message } = validated.data
-  const { error } = await resend.emails.send({
-    from: "Portfolio Website <marcos@marcosvelasco.com>",
-    to: process.env.RESEND_CONTACT_EMAIL!,
-    subject: `${name} sent you a message from your portfolio website.`,
-    reply_to: email,
-    react: EmailTemplate(validated.data),
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-  })
+  // const { name, email, message } = validated.data
+  // const { error } = await resend.emails.send({
+  //   from: "Portfolio Website <marcos@marcosvelasco.com>",
+  //   to: process.env.RESEND_CONTACT_EMAIL!,
+  //   subject: `${name} sent you a message from your portfolio website.`,
+  //   reply_to: email,
+  //   react: EmailTemplate(validated.data),
+  //   text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  // })
 
-  if (error) {
-    return {
-      status: 400,
-      success: false,
-      error: {
-        message: "Message failed to send.",
-        errors: error,
-      },
-    }
-  }
+  // if (error) {
+  //   return {
+  //     status: 400,
+  //     success: false,
+  //     error: {
+  //       message: "Message failed to send.",
+  //       errors: error,
+  //     },
+  //   }
+  // }
 
-  return {
-    status: 200,
-    success: true,
-    message: "Message sent successfully.",
-    error: null,
-  }
+  // return {
+  //   status: 200,
+  //   success: true,
+  //   message: "Message sent successfully.",
+  //   error: null,
+  // }
 }
