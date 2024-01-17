2d539c88c5acbb7960c3a8a5f8227659dc19251c
Marcos Velasco <devslife7@gmail.com>
2024-01-16 20:54:15 -0500

    server

--- app/actions/contact.ts
+++ app/actions/contact.ts
@@ -8,22 +8,20 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 export default async function sendMessage(formData: unknown) {
   // server side validation
-  const validated = ContactFormSchema.safeParse(formData)
-  if (!validated.success) {
-    return {
-      status: 400,
-      success: false,
-      error: {
-        message: "Message failed validate",
-        errors: validated.error.issues,
-      },
-    }
-  }
-
+  // const validated = ContactFormSchema.safeParse(formData)
+  // if (!validated.success) {
+  //   return {
+  //     status: 400,
+  //     success: false,
+  //     error: {
+  //       message: "Message failed validate",
+  //       errors: validated.error.issues,
+  //     },
+  //   }
+  // }
   // verify recaptcha
   // const captchaResp = await verifyReCaptcha(validated.data.recaptcha)
   // if (!captchaResp.success) return captchaResp
-
   // send email using the resend api
   // const { name, email, message } = validated.data
   // const { error } = await resend.emails.send({
@@ -34,7 +32,6 @@ export default async function sendMessage(formData: unknown) {
   //   react: EmailTemplate(validated.data),
   //   text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
   // })
-
   // if (error) {
   //   return {
   //     status: 400,
@@ -45,7 +42,6 @@ export default async function sendMessage(formData: unknown) {
   //     },
   //   }
   // }
-
   // return {
   //   status: 200,
   //   success: true,
